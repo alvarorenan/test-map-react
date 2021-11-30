54d8deae16c902fb6de75c3e95d7c5842380a584
Fernando Ferreira de Lima Filho <fferreiralf@ufrn.edu.br>
2021-11-30 11:40:19 -0300

    adicionar modal

--- main.js
+++ main.js
@@ -21,28 +21,53 @@ const map = new Map({
   })
 });
 
-map.on("click", (e) => {
-  const layer = new Feature({
-    geometry: new Point(fromLonLat(transform(e.coordinate, 'EPSG:3857', 'EPSG:4326'))),
-  });
+const mapModal = new Map({
+  target: 'mapModal',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: fromLonLat([-35.198429, -5.836685]),
+    zoom: 4.5
+  })
+});
 
-  layer.setStyle( new Style({
-    image: new Icon({
-      color: 'rgba(255, 255, 255, )',
-      crossOrigin: 'anonymous',
-      src: './assets/icon.png',
-      scale: 0.01,
-    }),
-  }));
+let btnAdicionar = document.getElementById("btnAdicionar");
 
-  const vectorSource = new VectorSource({
-    features: [layer],
-  });
+btnAdicionar.addEventListener("click", () => {
+  let container = document.getElementById("container");
+  container.style = "  visibility: visible;"
+});
 
-  const vectorLayer = new VectorLayer({
-    source: vectorSource,
-  });
-
-  map.addLayer(vectorLayer);
+mapModal.on("click", (e) => {
+  let point =  new Point(fromLonLat(transform(e.coordinate, 'EPSG:3857', 'EPSG:4326')));
+  console.log(point.getRevision());
+  point.changed()
+  // if(point.getRevision() == 0){
+    const layer = new Feature({
+      geometry: point
+    });
+  
+    layer.setStyle( new Style({
+      image: new Icon({
+        color: 'rgba(255, 255, 255, )',
+        crossOrigin: 'anonymous',
+        src: './assets/icon.png',
+        scale: 0.01,
+      }),
+    }));
+  
+    const vectorSource = new VectorSource({
+      features: [layer],
+    });
+  
+    const vectorLayer = new VectorLayer({
+      source: vectorSource,
+    });
+    mapModal.set(vectorLayer);  
+  // }
+  
 })
 
