a70eb59d9a7089d73d4510de5ee06956117dc352
martinsspn <martinsspn@gmail.com>
2021-12-03 12:04:21 -0300

    correção de componentes e continuação de refatoração para o react

--- src/App.js
+++ src/App.js
@@ -1,42 +1,15 @@
 import './App.css';
-import Map from "./components/Map";
-import Modal from "./components/Modal";
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
-  function btnAdicionar() {
-    let container = document.getElementById("container");
-    container.style = "  visibility: visible;"
-  }
-  function btnAll() {
-    
-  }
-  function btnSantorini() {
-    
-  }
-  function btnAcampamento() {
-    
-  }
+  
   return (
   <body>
-    <header>
-      <img src="./assets/logo.png" alt=""/>     
-    </header>
-    
-    <main >
-      <div class="btn-group">
-        <button type="button" id="btnAll" class="btn btn-success">Todos</button>
-        <button type="button" id="btnSantorini" class="btn btn-primary">Igrejas</button>
-        <button type="button" id="btnAcampamento" class="btn btn-danger">Acampamentos</button>
-      </div>
-      <div id="container">
-        <Modal />
-      </div>
-
-      <button id="btnAdicionar" class="btn" onClick={btnAdicionar}>
-        <img id="btnImg" src="./assets/icon.png" class="btnIcon" alt=""/>
-      </button>
-      <Map />
-    </main>  
+    <Header />
+    <Main />
+    <Footer/>  
   </body>
   );
 }
